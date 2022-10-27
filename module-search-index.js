2a0eaf871733ce23ec6bba7a5135fb93fea8cbe8
sttk <sttk@users.noreply.github.com>
2022-10-27 04:24:11 +0000

    deploy: 2120fcb0f7389c419b8b7efcbdd23ba46fb152bb

--- module-search-index.js
+++ module-search-index.js
@@ -1,1 +1,1 @@
-moduleSearchIndex = [];updateSearchResults();
+moduleSearchIndex = [{"l":"sabi"}];updateSearchResults();
--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sabi"},{"l":"sabi.nofity"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"sabi","l":"sabi"}];updateSearchResults();
