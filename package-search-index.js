9c000040feab66312118cd35acadd1c2e16c3486
sttk <sttk@users.noreply.github.com>
2025-07-13 05:32:07 +0000

    deploy: 7f0993fcf57819011b766f82a963cf0ec1dffa8c

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.github.sttk.sabi","l":"com.github.sttk.sabi"},{"m":"com.github.sttk.sabi","l":"com.github.sttk.sabi.errs"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.github.sttk.sabi","l":"com.github.sttk.sabi"}];updateSearchResults();
