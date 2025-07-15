c4ac20190d49d562159e3ea38ed86ff6fe9ff296
sttk <sttk.xslet@gmail.com>
2025-07-15 13:08:04 +0900

    0.5.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@
  *
  * <p>This module includes the interfaces that abstracts data accesses to the external data stores
  * and the classes to execute a logic function with or without transaction operations.
+ *
+ * @version 0.5
  */
 module com.github.sttk.sabi {
   exports com.github.sttk.sabi;
